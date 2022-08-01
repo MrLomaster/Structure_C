919fd1cf63c87e54feefc0544378f5a992638fe1
MrLomaster <timofeikalinin123@gmail.com>
2022-08-02 01:19:22 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿People one = new();
+Console.WriteLine("Первый");
+one.name = Console.ReadLine();
+one.mark = Console.ReadLine();
+Console.WriteLine("\t");
+
+People two = new();
+Console.WriteLine("Второй");
+two.name = Console.ReadLine();
+two.mark = Console.ReadLine();
+Console.WriteLine("\t");
+
+People three = new();
+Console.WriteLine("Третий");
+three.name = Console.ReadLine();
+three.mark = Console.ReadLine();
+Console.WriteLine("\t");
+
+one.Print_All();
+struct People
+{
+    public string name;
+    public string mark;
+
+    public void Print_Name() => Console.Write("{name}");
+    public void Print_Mark() => Console.Write("{mark}");
+    public void Print_All()
+    {
+        Console.Write(name + " ");
+        Console.Write(mark);
+    }
+
+}
